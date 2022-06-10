473dd0eddf7fe062fe36bb4bb506b815cc09930b
lucaswells <lucas@holtzforestry.com>
2022-06-10 14:24:17 -0600

    Include same_interval boolean in constructor of IgnitionCrew and implement validator Fixes #2

--- driptorch/errors.py
+++ driptorch/errors.py
@@ -3,9 +3,13 @@ class GeojsonError(Exception):
     read_error = 'Invalid GeoJSON format, that is all we know'
 
 
-class UnequalIgniterVelocity(Exception):
+class IgniterError(Exception):
     unequal_velocities = 'Cannot add igniter to crew due to unequal velocities. ' \
         'You can set `same_velocity=False` in the constructor, ' \
         'however this can raise subsequent errors if an ignition ' \
         'crew with unequal velocity is passed to an internal ignition ' \
         'pattern generation method.'
+    unequal_rates = 'Cannot add igniter to crew due to unequal rates. ' \
+        'You can set `same_rate=False` in the constructor, ' \
+        'however this can raise subsequent errors if an ignition ' \
+        'crew with unequal rates is passed to a fire module exporter.'
