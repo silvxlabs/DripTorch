e035e59d3364d09b9029fb5a3ed7bfca12e9a908
Frankie Dunbar <franklyn.dunbar@umontana.edu>
2022-10-10 12:24:59 -0600

    Pattern functionality done.

--- tests/test_0_io.py
+++ tests/test_0_io.py
@@ -24,96 +24,140 @@ class objects and their associated methods.
 To run these tests, call "pytest -ss -v" from the terminal.
 """
 
+SIMULATION_PATH = "resources/simulation_0.json"
+QF_VALIDATION_DATA = "resources/quicfire_output_test.dat"
+
+
 def test_geojson_io() -> None:
-    """Test geoJSON io functionality 
-    """
+    """Test geoJSON io functionality"""
     test_polygon_4326: Polygon = read_geojson_polygon(testgeoms.test_polygon)
-    polygon_points = np.array(testgeoms.test_polygon["features"][0]["geometry"]["coordinates"][0])
-    test_bounds = (np.min(polygon_points[:,0]),np.min(polygon_points[:,1]),np.max(polygon_points[:,0]),np.max(polygon_points[:,1]))
+    polygon_points = np.array(
+        testgeoms.test_polygon["features"][0]["geometry"]["coordinates"][0]
+    )
+    test_bounds = (
+        np.min(polygon_points[:, 0]),
+        np.min(polygon_points[:, 1]),
+        np.max(polygon_points[:, 0]),
+        np.max(polygon_points[:, 1]),
+    )
 
     assert test_polygon_4326.bounds == test_bounds
-    geojson_from_Polygon = write_geojson([test_polygon_4326],4326)
+    geojson_from_Polygon = write_geojson([test_polygon_4326], 4326)
 
     # Test recreated geoJSON for order
     test_a = geojson_from_Polygon["features"][0]["geometry"]["coordinates"][0]
     test_b = testgeoms.test_polygon["features"][0]["geometry"]["coordinates"][0]
-    
-    assert_array_almost_equal(test_a,test_b,decimal=5,err_msg="\nTest polygon and validation polygon are not aligned\n")
 
-    
-    
+    assert_array_almost_equal(
+        test_a,
+        test_b,
+        decimal=5,
+        err_msg="\nTest polygon and validation polygon are not aligned\n",
+    )
+
+
 def test_projector() -> None:
-    """Test the functionality of the Projector.forward(),Projector.backward(), and Projector.estimate_utm_epsg()
-    """
+    """Test the functionality of the Projector.forward(),Projector.backward(), and Projector.estimate_utm_epsg()"""
 
-    location = {"lat":46.86028,"lon":-113.98278}
+    location = {"lat": 46.86028, "lon": -113.98278}
     location_UTM_epsg = 32612
     src_epsg = 4326
-    dst_epsg: float = Projector.estimate_utm_epsg(**location) # Estimate UTM EPSG code from lat and lon position
+    dst_epsg: float = Projector.estimate_utm_epsg(
+        **location
+    )  # Estimate UTM EPSG code from lat and lon position
 
     # Test Projector.estimate_utm_epsg
     assert dst_epsg == location_UTM_epsg
 
-    projecter = Projector(src_epsg=src_epsg,dst_epsg=dst_epsg)
-    test_polygon_4326: Polygon = read_geojson_polygon(testgeoms.test_polygon) # Creat Polygon object from geoJSON
-    test_polygon_4326_to_UTM: Polygon = projecter.forward(test_polygon_4326) # Project from lat/lon to UTM
-    test_polygon_4326_to_UTM_to_4326: Polygon = projecter.backward(test_polygon_4326_to_UTM) # Project from UTM to lat/lon
+    projecter = Projector(src_epsg=src_epsg, dst_epsg=dst_epsg)
+    test_polygon_4326: Polygon = read_geojson_polygon(
+        testgeoms.test_polygon
+    )  # Creat Polygon object from geoJSON
+    test_polygon_4326_to_UTM: Polygon = projecter.forward(
+        test_polygon_4326
+    )  # Project from lat/lon to UTM
+    test_polygon_4326_to_UTM_to_4326: Polygon = projecter.backward(
+        test_polygon_4326_to_UTM
+    )  # Project from UTM to lat/lon
 
-    # Generate testing objects. Note that projection operations cannot maintain floating point precision 
+    # Generate testing objects. Note that projection operations cannot maintain floating point precision
     # above that of the input points which warrants rounding down.
 
     test_a = test_polygon_4326.bounds
     test_b = test_polygon_4326_to_UTM_to_4326.bounds
 
     # Test Projector.forward and Projector.backward
-    assert_array_almost_equal(test_a,test_b,decimal=5,err_msg="\nTest polygon and rectified polygon are not aligned\n")
+    assert_array_almost_equal(
+        test_a,
+        test_b,
+        decimal=5,
+        err_msg="\nTest polygon and rectified polygon are not aligned\n",
+    )
 
-    
+
 def test_web_mercator_funcs() -> None:
-    """Test web mercator functionality
-    """
+    """Test web mercator functionality"""
 
     test_polygon_4326: Polygon = read_geojson_polygon(testgeoms.test_polygon)
-    utm_epsg, test_polygon_4326_to_UTM = Projector.web_mercator_to_utm(test_polygon_4326)
-    test_polygon_4326_to_UTM_to_4326 = Projector.to_web_mercator(test_polygon_4326_to_UTM,utm_epsg)
+    utm_epsg, test_polygon_4326_to_UTM = Projector.web_mercator_to_utm(
+        test_polygon_4326
+    )
+    test_polygon_4326_to_UTM_to_4326 = Projector.to_web_mercator(
+        test_polygon_4326_to_UTM, utm_epsg
+    )
 
-    # Generate testing objects. Note that projection operations cannot maintain floating point precision 
+    # Generate testing objects. Note that projection operations cannot maintain floating point precision
     # above that of the input points which warrants rounding down.
     test_a = test_polygon_4326.bounds
     test_b = test_polygon_4326_to_UTM_to_4326.bounds
 
     # Test Projector.forward and Projector.backward
-    assert_array_almost_equal(test_a,test_b,decimal=5,err_msg="\nTest polygon and rectified polygon are not aligned\n")
+    assert_array_almost_equal(
+        test_a,
+        test_b,
+        decimal=5,
+        err_msg="\nTest polygon and rectified polygon are not aligned\n",
+    )
 
 
-
-    
 def test_write_quickfire() -> None:
-    """Test io.write_quicfire()
-    """
-    validation_data_path = path.join(path.dirname(__file__),"resources/simulation_0.json")
-    with open(validation_data_path,"r") as file:
+    """Test io.write_quicfire()"""
+    validation_data_path = path.join(path.dirname(__file__), SIMULATION_PATH)
+    with open(validation_data_path, "r") as file:
         validation_data = json.load(file)
-        pattern = dt.pattern.Pattern.from_dict(validation_data["ring_pattern"],epsg=validation_data["epsg"])
-        burn_unit = dt.unit.BurnUnit.from_json(validation_data["burn_unit"],wind_direction=validation_data["args"]["wind_direction"])
-        lower_left = validation_data["lower_left"]
-        times = pattern.times
-        geometries = pattern.geometry
-        elapsed_time = pattern.elapsed_time
+    pattern = dt.pattern.Pattern.from_dict(
+        validation_data["ring_pattern"], epsg=validation_data["epsg"]
+    )
+    lower_left = validation_data["lower_left"]
+    times = pattern.times
+    geometries = pattern.geometry
+    elapsed_time = pattern.elapsed_time
 
-    #Translate geometries from the firing technique t
+    # Translate geometries from the firing technique t
     trans_geoms = []
     for geom in geometries:
-        trans_geoms.append(affinity.translate(
-            geom, -lower_left[0], -lower_left[1]))
+        trans_geoms.append(affinity.translate(geom, -lower_left[0], -lower_left[1]))
     geometries = trans_geoms
 
     # Generate quicfire output
-    quicfire_output = write_quicfire(geometry=geometries,times=times,elapsed_time=elapsed_time,resolution=1)
-    test_quicfire_path = path.join(path.dirname(__file__), "resources/quicfire_output_test.dat")
-    
-    with open(test_quicfire_path,"r") as test_quicfire_output:
+    quicfire_output = write_quicfire(
+        geometry=geometries, times=times, elapsed_time=elapsed_time, resolution=1
+    )
+    test_quicfire_path = path.join(path.dirname(__file__), QF_VALIDATION_DATA)
+
+    with open(test_quicfire_path, "r") as test_quicfire_output:
         # Truncate for speed
-        test_a = '\n'.join(test_quicfire_output.readlines()).replace("\n\n","\n").split("/")[1].strip("\n").split(" ")[:20]
-        test_b = quicfire_output.replace("\n\n","\n").split("/")[1].strip("\n").split(" ")[:20]
+        test_a = (
+            "\n".join(test_quicfire_output.readlines())
+            .replace("\n\n", "\n")
+            .split("/")[1]
+            .strip("\n")
+            .split(" ")[:20]
+        )
+        test_b = (
+            quicfire_output.replace("\n\n", "\n")
+            .split("/")[1]
+            .strip("\n")
+            .split(" ")[:20]
+        )
     assert test_a == test_b
---
+++ tests/test_2_pattern.py
@@ -0,0 +1,117 @@
+# External Imports
+from shapely import affinity
+from shapely.geometry.polygon import Polygon
+from shapely.geometry import shape
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+import json
+
+# Core Imports
+import os
+import os.path as path
+from datetime import datetime
+import itertools
+import tempfile
+
+# Internal Imports
+import driptorch as dt
+from tests.resources import simulations
+
+"""
+The following defined functions are for testing class objects of pattern.py.
+
+We use the construct of "Validation data" for previously generated data,
+and the construct of "Test data" for data generated at run time.
+
+Test data is compared against validation data to assert the functionality of
+class objects and their associated methods.
+
+To run these tests, call "pytest -ss -v" from the terminal.
+"""
+
+SIMULATION_PATH = "resources/simulation_0.json"
+QF_VALIDATION_PATH = "resources/quicfire_output_test.dat"
+
+
+def test_pattern_io() -> None:
+    validation_data_path = path.join(path.dirname(__file__), SIMULATION_PATH)
+    qf_validation_data_path = path.join(path.dirname(__file__), QF_VALIDATION_PATH)
+
+    with open(validation_data_path, "r") as file:
+        validation_data = json.load(file)
+
+    with open(qf_validation_data_path, "r") as file:
+        qf_validation_data = "\n".join(file.readlines())
+
+    test_pattern_0 = dt.pattern.Pattern.from_dict(
+        validation_data["head_pattern"], epsg=validation_data["epsg"]
+    )
+    test_pattern_1 = dt.pattern.Pattern.from_dict(
+        validation_data["ring_pattern"], epsg=validation_data["epsg"]
+    )
+    test_burn_unit = dt.BurnUnit.from_json(
+        validation_data["burn_unit"],
+        wind_direction=validation_data["args"]["wind_direction"],
+    )
+
+    # Test Pattern.to_dict()
+    test_a = test_pattern_0.to_dict()["times"]
+    test_b = validation_data["head_pattern"]["times"]
+    assert test_a == test_b
+
+    merge_0 = test_pattern_0.merge(test_pattern_1)
+    merge_1 = test_pattern_1.merge(test_pattern_0)
+
+    test_a = sorted([x.bounds for x in merge_0.geometry])
+    test_b = sorted([x.bounds for x in merge_1.geometry])
+
+    assert_array_almost_equal(
+        test_a, test_b, decimal=5, err_msg="\nMerged pattern geometries not aligned.\n"
+    )
+
+    test_a = merge_0.elapsed_time
+    test_b = merge_1.elapsed_time
+
+    assert_array_almost_equal(
+        test_a,
+        test_b,
+        decimal=5,
+        err_msg="\nMerged pattern elapsed times are not equivalent.\n",
+    )
+
+    x_off = 100
+    y_off = 100
+
+    translated = test_pattern_1.translate(x_off=x_off, y_off=y_off)
+    un_translated = translated.translate(x_off=-1 * x_off, y_off=-1 * y_off)
+
+    test_a = [
+        x.coords
+        for x in list(
+            itertools.chain.from_iterable([x.geoms for x in test_pattern_1.geometry])
+        )
+    ]
+
+    test_b = [
+        x.coords
+        for x in list(
+            itertools.chain.from_iterable([x.geoms for x in un_translated.geometry])
+        )
+    ]
+
+    assert_array_almost_equal(
+        test_a,
+        test_b,
+        decimal=5,
+        err_msg="\nTranslated geometries are not aligned.\n",
+    )
+
+    test_a = qf_validation_data.split("/")[-1].split("\n").remove("")
+    # Write pattern to quicfire and then open it back up
+    fd, pth = tempfile.mkstemp()
+    test_pattern_1.to_quicfire(test_burn_unit, filename=pth)
+    with open(pth, "r") as file:
+        test_b = "\n".join(file.readlines()).split("/")[-1].split("\n").remove("")
+    os.close(fd)
+
+    assert test_a == test_b
