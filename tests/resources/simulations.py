dde13223c4617aa29291df7b8015b17acdc7e789
Frankie Dunbar <franklyn.dunbar@umontana.edu>
2022-10-11 16:14:53 -0600

    rough draft for patching functions

--- tests/resources/simulations.py
+++ tests/resources/simulations.py
@@ -1,3 +1,9 @@
+# Core imports
+from datetime import datetime
+import json
+
+# Internal imports
+import driptorch as dt
 from .testgeoms import test_polygon
 
 simulation_args = {
@@ -10,3 +16,37 @@ simulation_args = {
     "number_igniters": 2,
     "offset": 50
 }
+
+def patch_simulation(simulation_path:str, input_data:dict) -> None:
+    """Patch a given simulation data set with input data. Marks the time, current Drip Torch Verion
+       and updated fields
+
+    Args:
+        simulation_path (str): Path to the simulation data
+        input_data (dict): Data to be patched into the simulation formated as {Field:Data}
+    """
+
+    simulation_data_path = path.join(path.dirname(__file__), simulation_path)
+    with open(simulation_data_path, "r") as file:
+        simulation_data = json.load(file)
+
+    fields = list(input_data.keys())
+    patch_data = {
+        "date": datetime.now().isoformat(),
+        "version": dt.__version__,
+        "fields":fields
+    }
+    try:
+        simulation_data["patch_history"].append(patch_data)
+    except KeyError:
+        simulation_data["patch_history"] = [patch_data]
+
+    for k,v in input_data.items():
+        try:
+            simulation_data[k] = json.dumps(v)
+        except Error as e:
+            print(f"\n Error: {e} \n Patching to {simulation_path} failed for {k}\n")
+
+    with open(simulation_data_path, "w") as file:
+        json.dump(simulation_data, file)
+    
