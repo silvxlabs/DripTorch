520679c337923cc2060e416f3428962b5fb89a1b
Frankie Dunbar <franklyn.dunbar@umontana.edu>
2022-10-12 10:35:21 -0600

    patch functionality and update igniter/firing VD

--- tests/resources/simulations.py
+++ tests/resources/simulations.py
@@ -17,36 +17,3 @@ simulation_args = {
     "offset": 50
 }
 
-def patch_simulation(simulation_path:str, input_data:dict) -> None:
-    """Patch a given simulation data set with input data. Marks the time, current Drip Torch Verion
-       and updated fields
-
-    Args:
-        simulation_path (str): Path to the simulation data
-        input_data (dict): Data to be patched into the simulation formated as {Field:Data}
-    """
-
-    simulation_data_path = path.join(path.dirname(__file__), simulation_path)
-    with open(simulation_data_path, "r") as file:
-        simulation_data = json.load(file)
-
-    fields = list(input_data.keys())
-    patch_data = {
-        "date": datetime.now().isoformat(),
-        "version": dt.__version__,
-        "fields":fields
-    }
-    try:
-        simulation_data["patch_history"].append(patch_data)
-    except KeyError:
-        simulation_data["patch_history"] = [patch_data]
-
-    for k,v in input_data.items():
-        try:
-            simulation_data[k] = json.dumps(v)
-        except Error as e:
-            print(f"\n Error: {e} \n Patching to {simulation_path} failed for {k}\n")
-
-    with open(simulation_data_path, "w") as file:
-        json.dump(simulation_data, file)
-    
