ef33852e08bfc19c76aee8fb7cdc10a16848d3d4
Frankie Dunbar <franklyn.dunbar@umontana.edu>
2022-11-01 14:21:21 -0600

    debuging contour

--- driptorch/firing/strip.py
+++ driptorch/firing/strip.py
@@ -139,3 +139,84 @@ class Strip(FiringBase):
                 direction_toggle ^= True
 
         return paths
+    
+    def raw_paths(self, paths: dict, **kwargs) -> dict:
+        """Initialize spatial part of the ignition paths.
+
+        Notes:
+            Overrides the `_init_paths()` method in the base class.
+
+        Args:
+            paths (dict): Empty pattern path dictionary
+
+        Returns:
+            dict: Pattern path dictionary with initial untimed paths
+        """
+
+        # Get the depth parameter from the keyword args (This is required in the
+        # `generate_pattern()` method of this class)
+        depth = kwargs['depth']
+        heat_depth = kwargs['heat_depth']
+        side = kwargs['side']
+
+        # Extract the bounding extent of the firing area
+        bbox = self._burn_unit.bounds
+        x_min, y_min = bbox[:, 0].min(), bbox[:, 1].min()
+        x_max, y_max = bbox[:, 0].max(), bbox[:, 1].max()
+
+        # If depth=None, compute depth by equally spacing ignitors. This make sense for flank
+        # technique since we don't what ignitors walking back downwind in the fire
+        # they just ignited.
+        if not depth:
+            depth = (y_max - y_min) / (len(self._ignition_crew) + 1)
+
+        # Set up the initial start positions along the y-axis
+        # If no heat depth is specified, then we have constant spacing between igniters and heats
+        if not heat_depth:
+            y_range = np.arange(y_min + depth, y_max, depth)
+        # If a heat depth is specified, then we have constant spacing between igniters,
+        # but potentially a different spacing between heats.
+        else:
+            y_range = []
+            cur_y = y_min + depth
+            i = 0
+            while cur_y < y_max:
+                y_range.append(cur_y)
+                if (i+1) % len(self._ignition_crew) == 0:
+                    cur_y = y_range[i] + heat_depth
+                else:
+                    cur_y = y_range[i] + depth
+                i += 1
+
+        # Flip the y_range if we're on the right side
+        if side == 'left':
+            y_range = y_range[::-1]
+
+        # Initialize loop control parameters
+        cur_heat = 0
+        cur_igniter = 0
+
+        # For each start position, build a path and assign to a heat and igniter
+        for i, y in enumerate(y_range):
+
+            # Build the path and clip to firing area boundary
+            line = LineString(((x_min, y), (x_max, y)))
+
+            # Get lines or multipart lines in the same list structure for looping below
+
+            line_list = [line]
+
+
+            # Assign the path to a heat, igniter and leg
+          
+            paths['heat'].append(cur_heat)
+            paths['igniter'].append(cur_igniter)
+            paths['geometry'].append(line)
+
+            # Update loop control parameters
+            cur_igniter += 1
+            if (i+1) % len(self._ignition_crew) == 0:
+                cur_igniter = 0
+                cur_heat += 1
+
+        return paths
