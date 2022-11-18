7ba1f6f6af6d50fe2d30bb1f35a1c4a4e8dab5b1
Frankie Dunbar <franklyn.dunbar@umontana.edu>
2022-11-18 13:08:21 -0700

    testing structure

--- cost_distance.py
+++ cost_distance.py
@@ -27,10 +27,14 @@ class Raster:
         self.rows,self.cols = raster.shape
         self.raster = raster.flatten()
         self.inds = np.arange(self.raster.shape[0])
-        self.neighbor_Kernel = [-self.cols-1,-self.cols,-self.cols+1,-1,1,self.cols-1,self.cols,self.cols+1] 
-        if transform:
+        self.neighbor_Kernel = [-self.cols-1,-self.cols,-self.cols+1,-1,1,self.cols-1,self.cols,self.cols+1]
+        
+        self._world2ind = None
+        self._ind2world = None
+        if transform is not None:
             self._world2ind = transform.copy()
             self._ind2world = np.linalg.inv(transform)
+        
 
     def get_Index(self,index:int) -> np.ndarray:
         # Return the 2d index given the 1d index
@@ -84,9 +88,9 @@ class Raster:
     def __setitem__(self,key,item):
         self.raster[key] = item
 
-    def world2ind(location,round=True) -> np.ndarray:
+    def world2ind(self,location,round=True) -> np.ndarray:
         # Map from spatial coordinates to raster coords
-        if self._world2ind:
+        if self._world2ind is not None:
             point = [location[0],location[1],1]
             index = self._world2ind@point
             index = index[:-1]
@@ -96,9 +100,9 @@ class Raster:
         else:
             return None 
 
-    def ind2world(index)-> np.ndarray:
+    def ind2world(self,index)-> np.ndarray:
         # Map from raster coordinates to world coords
-        if self._ind2world:
+        if self._ind2world is not None:
             point = [index[0],index[1],1]
             world = self._ind2world@point
             return world[:-1]
@@ -107,8 +111,8 @@ class Raster:
 
 
 class ElevationRaster(Raster):
-    def __init__(self,raster:np.ndarray):
-        super().__init__(raster=raster)
+    def __init__(self,raster: np.ndarray, transform: np.ndarray):
+        super().__init__(raster=raster,transform=transform)
 
     def generate_source_cost(self,path:list) -> np.ndarray:
         """Generate a source array given a starting path, where the 
@@ -120,22 +124,22 @@ class ElevationRaster(Raster):
         Returns:
             np.ndarray: _description_
         """
-
+        
         start,stop = path[:2],path[2:]
         start_mat,stop_mat = self.world2ind(start),self.world2ind(stop)
         if self.rows - start_mat[0] > self.rows//2:
             source_slice = np.s_[-1,:]
         else:
             source_slice = np.s_[0,:]
-        source_array = np.zeros_like(self.raster).astype(bool)
+        source_array = np.zeros((self.rows,self.cols)).astype(bool)
         source_array[source_slice] = True
 
-        cost_raster = np.ones_like(self.raster)
+        cost_raster = np.ones((self.rows,self.cols))
         cost_raster *= np.inf
         cost_raster[source_slice] = 0
 
         cost_raster = Raster(cost_raster)
-        source_raster = SourceRaster(raster)
+        source_raster = SourceRaster(source_array)
 
         return cost_raster,source_raster
 
@@ -150,13 +154,10 @@ class SourceRaster(Raster):
 
 
 class CostDistance:
-    def __init__(self,raw_paths:dict,elevation_raster:np.ndarray):
-
-        self.elevation_raster = ElevationRaster(elev_raster)
+    def __init__(self,raw_paths:dict,elevation_raster:ElevationRaster):
+        self.elevation_raster = elev_raster
         self.raw_paths = raw_paths
-        self.cost_raster,self.source_raster = self.elevation_raster.generate_source_cost(self.raw_paths["geometry"][0])
-       
-     
+        self.cost_raster,self.source_raster = self.elevation_raster.generate_source_cost(self.raw_paths["geometry"][0].bounds)
         source_neighbors = [self.source_raster.get_Neighbors(index) for index in  self.source_raster.locations]
         init_costs = self._compute_costs(source_neighbors)
         self.PQ = Heap(init_costs)
@@ -173,7 +174,6 @@ class CostDistance:
         return computed_costs
     
     def iterate(self,num_igniters,igniter_depth,heat_depth) -> np.ndarray:
-
         # Generate cost surface
         while len(self.PQ.list) > 0:
             cost,loc = self.PQ.pop()
@@ -186,60 +186,98 @@ class CostDistance:
                 for c in neighbor_costs:
                     self.PQ.push(c)
         cost_surface = self.cost_raster.raster.reshape((self.cost_raster.rows,self.cost_raster.cols))
-
         levels = [igniter_depth]
         while levels[-1] < np.max(cost_surface):
             for i in range(num_igniters - 1):
                 levels.append(levels[-1] + igniter_depth)
             levels.append(levels[-1] + heat_depth)
-
-        contours = plt.contour(cost_surface, colors='w', lw='0.25', levels=levels)
-        
+        contours = plt.contour(cost_surface,levels=levels)
         paths = []
         for i,p in enumerate(contours.collections[0].get_paths()):
             if i == 0:
                 current_heat = i//num_igniters
                 heat_set = []
-        
             heat = i//num_igniters
             if heat > current_heat:
                 current_heat = heat
                 paths.append(heat_set)
                 heat_set = []
-            
             else:
                 heat_set.append(
                     p.vertices
                 )
-                
         return paths,cost_surface
             
         
 
 if __name__ == "__main__":
+    
+    # External Imports
+
+    import numpy as np 
     import matplotlib.pyplot as plt
+    from scipy.stats import norm
+    import shapely
+    from shapely.geometry import Polygon,Point
 
+    # Core Imports
+    import json
+    import os.path as path
+    import sys
+    from collections import namedtuple
+
+    # Internal Imports
+    sys.path.append("/driptorch")
+    import driptorch as dt
+    from driptorch.firing import flank
+
+    # Define burn unit spatial data in GeoJSON format
     elevs = np.load("/Users/franklyndunbar/Project/testarea.npy")
-    source_raster = np.zeros_like(elevs).astype(bool)
-    cost_raster = np.ones_like(elevs)
-    cost_raster *= np.inf
-    source_slice = np.s_[0,:]
+    transform =  np.load("/Users/franklyndunbar/Project/bluemountain_dem_transform.npy")
 
-    source_raster[source_slice] = True
-    cost_raster[source_raster==True] =  0
+    blue_mountain = [ [ -114.115414416057106, 46.84141805647527 ], 
+    [ -114.115414291632106, 46.841120501959843 ], [ -114.115029536394999, 46.840770983423162 ], [ -114.114665457279102, 46.840548989245221 ], 
+    [ -114.114413512744093, 46.840076418364873 ], [ -114.114525405809005, 46.839461779184177 ], [ -114.114333139108197, 46.839058605593813 ], 
+    [ -114.113953012275601, 46.838563152635771 ], [ -114.113451259100998, 46.837883698065788 ], [ -114.113224837669506, 46.837587544981723 ], 
+    [ -114.112789079409595, 46.836901298964847 ], [ -114.112930132494895, 46.836593931018783 ], [ -114.115362037381402, 46.835906544594543 ], 
+    [ -114.115155840059799, 46.835699418591219 ], [ -114.114651512340302, 46.835676739527457 ], [ -114.113836748826202, 46.835695352398247 ], 
+    [ -114.113174153126593, 46.835779624664482 ], [ -114.112181587364802, 46.835899281750358 ], [ -114.111299220479907, 46.83588420728352 ], 
+    [ -114.110869729217498, 46.835757931029612 ], [ -114.110161683708398, 46.835556727734392 ], [ -114.109653672339604, 46.835491082423658 ], 
+    [ -114.108349803286202, 46.835471042038172 ], [ -114.107654022255502, 46.835554206149077 ], [ -114.107000851808294, 46.835709468827467 ], 
+    [ -114.106305895096099, 46.835728357206087 ], [ -114.105282095585196, 46.835833565874623 ], [ -114.1055753992798, 46.836516386443613 ], 
+    [ -114.105737911069895, 46.836894711956553 ], [ -114.106538569463297, 46.837331382367509 ], [ -114.1071623129843, 46.837737434373651 ], 
+    [ -114.107713413292899, 46.838129136519498 ], [ -114.107982567263093, 46.838551148632902 ], [ -114.108502357207598, 46.839075937037279 ], 
+    [ -114.108969268362799, 46.839671473567421 ], [ -114.109297059793505, 46.840260548147413 ], [ -114.109857244406001, 46.841438219047838 ], 
+    [ -114.115414416057106, 46.84141805647527 ] ] 
+    blue_mountain = Polygon([Point(p) for p in blue_mountain])
+    burn_unit = dt.BurnUnit(blue_mountain, firing_direction=0)
+    bounds = burn_unit.bounds
+    firing_area = burn_unit.buffer_control_line(5)
+    firing_area = firing_area.buffer_downwind(20)
+    blackline_area = burn_unit.difference(firing_area)
+    dash_igniter = dt.Igniter(5,dash_length=.5)
 
-    elev_raster = Raster(elevs)
-    cost_raster = Raster(cost_raster)
-    source_raster = SourceRaster(source_raster)
+    num_igniters = 5
+    igniter_spacing = 5
+    igniter_depth = 5
+    heat_spacing = 10
+    point_crew = dt.IgnitionCrew.clone_igniter(dash_igniter, num_igniters)
+    technique = dt.firing.Strip(firing_area, point_crew)
+    raw_paths = technique.raw_paths(paths=dt.Pattern.empty_path_dict(),spacing=igniter_spacing,depth=igniter_depth,heat_depth=heat_spacing,side='left')
 
-    CD = CostDistance(source_raster,cost_raster,elev_raster)
-    paths,test = CD.iterate(3,5,12)
+    elev_raster = ElevationRaster(raster=elevs,transform=transform)
+
+    CD = CostDistance(raw_paths=raw_paths,elevation_raster=elev_raster)
+    paths,test = CD.iterate(num_igniters,igniter_depth,heat_spacing)
+
+  
     np.save("cost_surface",test)
     plt.rcParams["figure.figsize"] = (160/8,90/8)
-    plt.imshow(test.reshape(cost_raster.rows,cost_raster.cols))
+    plt.imshow(test.reshape(elev_raster.rows,elev_raster.cols))
     for heat in paths:
         for igniter in heat:
             plt.plot(heat[:,0],heat[:,1])
     
     plt.colorbar()
     plt.show()
+ 
