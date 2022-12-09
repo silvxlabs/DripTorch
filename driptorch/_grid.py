c9a3d0a940082c98edf5ac89f755c74bb2faf92f
lucaswells <lucas@holtzforestry.com>
2022-12-09 08:39:46 -0700

    Extended Transform and Grid objects in grid module

--- driptorch/_grid.py
+++ driptorch/_grid.py
@@ -5,14 +5,14 @@ from __future__ import annotations
 from .io import Projector
 
 # External imports
-from shapely.geometry import Polygon, MultiPoint, MultiLineString
-import numpy as np
 import gcsfs
-import zarr
+import numpy as np
 from scipy.interpolate import griddata
 from scipy.ndimage import gaussian_filter
+from skimage import draw
 from skimage.measure import find_contours
-import pdb
+from shapely.geometry import Polygon, MultiPoint, LineString, MultiLineString
+import zarr
 
 
 class Transform:
@@ -39,67 +39,83 @@ class Transform:
         self.res_x = res_x
         self.res_y = res_y
 
-        self.build_map_matrix()
-
-    def build_map_matrix(self):
-        """Constructs the matrix coordinates to world coordinate transformation
-        """
-
-        self.ind2worldmatrix = np.array([
+        self.affine = np.array([
             [self.res_x, 0, self.upper_left_x],
             [0, self.res_y, self.upper_left_y],
             [0, 0, 1]
         ])
 
-        self.world2indmatrix = np.linalg.inv(self.ind2worldmatrix)
+        self.affine_inv = np.linalg.inv(self.affine)
 
-    def world2ind(self, locs: np.ndarray) -> np.ndarray:
-        """Maps an array of world coordinates to matrix coordinates
+    def get_matrix_indices(self, coords: np.ndarray) -> np.ndarray:
+        """Converts world coordinates to matrix coordinates
 
-        Args:
-            locs (np.ndarray): Array of world cordinates [[x,y]]
+        Parameters
+        ----------
+        coords : np.ndarray
+            Coordinate array
 
-        Returns:
-            np.ndarray: Array of matrix coordinates [[row,column]]
-        """
-        if len(locs.shape) < 2:
-            locs = locs.reshape(1, -1)
-        locs = np.hstack((locs, np.ones((locs.shape[0], 1))))
-        indicies = locs@self.world2indmatrix.T
-        return indicies.astype(int)
-
-    def ind2world(self, locs: np.ndarray) -> np.ndarray:
-        """Maps an array of matrix coordinates to world coordinates
-
-        Args:
-            locs (np.ndarray): Array of matrix coordinates [[row,column]]
-
-        Returns:
-            np.ndarray: Array of world cordinates [[x,y]]
+        Returns
+        -------
+        np.ndarray
+            Matrix coordinates
         """
 
-        if len(locs.shape) < 2:
-            locs = locs.reshape(1, -1)
-        locs = np.hstack((locs, np.ones((locs.shape[0], 1))))
-        worldpoints = locs@self.ind2worldmatrix.T
-        return worldpoints[:, :-1]
+        # Handle single coordinate case
+        if len(coords.shape) < 2:
+            coords = coords.reshape(1, -1)
+
+        # Add homogeneous coordinate and compute coordinates
+        coords = np.hstack((coords, np.ones((coords.shape[0], 1))))
+        indices = self.affine_inv.dot(coords.T).T
+
+        # Strip homogeneous coordinate and return
+        return indices[:, :-1].astype(int)
+
+    def get_world_coordinates(self, indices: np.ndarray) -> np.ndarray:
+        """Convert matrix indices to world coordinates
+
+        Parameters
+        ----------
+        indices : np.ndarray
+            Index array
+
+        Returns
+        -------
+        np.ndarray
+            World coordinates
+        """
+
+        # Handle single index case
+        if len(indices.shape) < 2:
+            indices = indices.reshape(1, -1)
+
+        # Add homogeneous coordinate and compute indices
+        indices = np.hstack((indices, np.ones((indices.shape[0], 1))))
+        coords = self.affine.dot(indices.T).T
+
+        # Strip homogeneous coordinate and return
+        return coords[:, :-1]
 
     @classmethod
-    def from_map_matrix(cls, map: np.ndarray) -> Transform:
-        """Builds Transform object from matrix coordinate to world coordinate transform
+    def from_affine(cls, affine_matrix: np.ndarray) -> Transform:
+        """Create a Transform object from an affine transformation matrix
 
-        Args:
-            map (np.ndarray): World coordinate to matrix coordinate transform
+        Parameters
+        ----------
+        affine_matrix : np.ndarray
+            Affine transformation matrix
 
-        Returns:
-            Transform : Transform object
+        Returns
+        -------
+        Transform
+            New Transform object
         """
 
-        res_x = np.abs(map[0][0])
-        res_y = np.abs(map[1][1])
-
-        ul_x = np.abs(map[0][-1])
-        ul_y = np.abs(map[1][-1])
+        res_x = affine_matrix[0, 0]
+        res_y = affine_matrix[1, 1]
+        ul_x = affine_matrix[0, 2]
+        ul_y = affine_matrix[1, 2]
 
         return Transform(ul_x, ul_y, res_x, res_y)
 
@@ -187,7 +203,7 @@ class Bounds:
 
 
 class Grid:
-    """Class for storing raster data"""
+    """Class for storing and manipulating raster data"""
 
     def __init__(self, data: np.ndarray | zarr.Array, transform: Transform, epsg: int):
         """Constructor
@@ -226,6 +242,17 @@ class Grid:
             self.cols * self.transform.res_x,
             self.transform.upper_left_y
         )
+
+    def copy(self) -> Grid:
+        """Copy the grid
+
+        Returns
+        -------
+        Grid
+            Copied grid
+        """
+
+        return Grid(self.data.copy(), self.transform, self.epsg)
 
     def extract_by_bounds(self, bounds: Bounds, padding: int = 0) -> Grid:
         """Extract a sub array by provided bounds object
@@ -310,7 +337,7 @@ class Grid:
 
         # Create a new grid space for the interpolated reprojected grid ata
         x = np.arange(dst_bounds.west, dst_bounds.east + dst_res, dst_res)
-        y = np.arange(dst_bounds.north, dst_bounds.south + dst_res, -dst_res)
+        y = np.arange(dst_bounds.north, dst_bounds.south - dst_res, -dst_res)
         xx, yy = np.meshgrid(x, y)
 
         # Interpolated the reprojected points to the new grid space
@@ -337,55 +364,117 @@ class Grid:
             List of contours
         """
 
-        if len(self.data.shape) < 2:
-            image = self.reshape()
-        else:
-            image = self.data
+        bounds = self.bounds
 
         # Loop over the levels and extract contours
         contours = []
         for level in levels:
+            isoline = find_contours(self.data, level)
 
-            # Go from image coords to matrix coords
-            isoline = list(map(
-                np.fliplr, find_contours(image[:, ::-1], level)
-            ))
-
-            lines = []
-            for line in isoline:
-                line = np.array(line)
-                line[:, 0] = image.shape[1] - line[:, 0]
-                lines.append(line)
-
-            # Map into world coordinates
-            isoline_world = list(map(
-                self.transform.ind2world, lines
-            ))
-
-            # Cast as MultiLineString
-            contours.append(
-                MultiLineString([line for line in isoline_world])
-            )
+            # The columns vectors in the 2xn array need to be flipped for (x,y), artifact
+            # from the skimage find_contours function.
+            # Also, we need to reflect along the y axis before translating to geo position.
+            # Also, we need to scale by the resolution defined in the transform.
+            # Also, this is a great example of how NOT to use list comprehensions.
+            contours.append(MultiLineString(
+                [(np.fliplr(i * np.array([self.transform.res_y, self.transform.res_x])).T).T +
+                 [bounds.west, bounds.south] for i in isoline]))
 
         return contours
 
-    def smooth(self, sigma: int):
-        """Smooths the raster data by applying a gaussian kernel
+    def pad(self, n: int, value: float | int | None = None):
+        """Add or subtract padding of `n` cells to the grid. If `n` is positive,
+        you can optionally specify a `value` to pad with.
 
-        Args:
-            sigma (int): Kernel standard deviation in pixels
+        Parameters
+        ----------
+        n : int
+            Number of cells to add to the boundary
+        value : float | int, optional
+            Fill value for the padded cells, by default 0.0
         """
-        self.data = self.reshape()
+
+        # Check if we're adding or subtracting the padding
+        if n > 0:
+            padded_data = np.pad(self.data, (n, n), constant_values=value)
+        elif n < 0:
+            padded_data = self.data[-n:n, -n:n]
+
+        # Recompute the transform
+        new_transform = Transform(
+            self.transform.upper_left_x - n * self.transform.res_x,
+            self.transform.upper_left_y + n * self.transform.res_y,
+            self.transform.res_x,
+            self.transform.res_y
+        )
+
+        return Grid(padded_data, new_transform, self.epsg)
+
+    @classmethod
+    def like(cls, grid: Grid, fill_value: float | int = 0) -> Grid:
+        """Create a new grid like an existing grid and fill with a constant value
+
+        Parameters
+        ----------
+        grid : Grid
+            Grid shape, transform and projection to copy
+        fill_value : float | int, optional
+            Value to fill the grid with, by default 0
+
+        Returns
+        -------
+        Grid
+            New grid
+        """
+
+        data = np.zeros_like(grid.data)
+
+        if fill_value != 0:
+            data[...] = fill_value
+
+        return cls(data, grid.transform, grid.epsg)
+
+    def draw_line(self, line: LineString, fill_value: float | int = 1) -> None:
+        """Rasterize a shapely LineString object
+
+        Parameters
+        ----------
+        line : LineString
+            Line string to rasterize
+        fill_value : float | int, optional
+            Value to assign to cells representing the line, by default 1
+        """
+
+        # Get the coordinate of the shapely LineString object
+        x, y = line.coords.xy
+
+        # Stack coords into a 2xn coordinate array and convert to matrix indices
+        geo_coords = np.array([x, y]).T
+        matrix_coords = self.transform.get_matrix_indices(geo_coords)
+
+        # Rasterize each segment in the line string
+        for i in range(0, matrix_coords.shape[0] - 1):
+            rr, cc = draw.line(
+                matrix_coords[i, 1], matrix_coords[i, 0], matrix_coords[i + 1, 1], matrix_coords[i + 1, 0])
+            self.data[rr, cc] = fill_value
+
+    def smooth(self, sigma: int):
+        """Apply a Gaussian filter to the grid
+
+        Parameters
+        ----------
+        sigma : int
+            Standard deviation of the Gaussian kernel
+        """
+
         self.data = gaussian_filter(self.data, sigma=sigma)
-        self.data = self.data.flatten()
 
 
 class AlbersConusDEM(Grid):
     """Child grid class for the Albers CONUS DEM"""
 
     def __init__(self):
-        """Constructor
-        """
+        """Constructor"""
 
         # Connect to the Albers DEM on GCS
         gcs = gcsfs.GCSFileSystem(token='anon')
